a5ec81a66a95ad0247c87b3389a7e4645498424e
Vishal p <vishal.p@codiste.com>
2023-02-25 18:15:40 +0530

    First Commit

---
+++ SepiaPets/SepiaPets/SepiaPetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SepiaPetsApp.swift
+//  SepiaPets
+//
+//  Created by codiste on 25/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SepiaPetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
